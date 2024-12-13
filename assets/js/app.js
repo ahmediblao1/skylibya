a6ec72bf4472a9e352eb7cd2ac6385bdb1223b27
cod4r iblao <ahmediblao53@gmail.com>
2024-12-14 02:39:26 +0300

    Initial commit

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,176 +1,343 @@
-import * as THREE from 'https://cdn.skypack.dev/three@0.129.0/build/three.module.js';
-import { GLTFLoader } from 'https://cdn.skypack.dev/three@0.129.0/examples/jsm/loaders/GLTFLoader.js';
-import { gsap } from 'https://cdn.skypack.dev/gsap';
+// clearing the console (just a CodePen thing)
 
-const camera = new THREE.PerspectiveCamera(
-    10,
-    window.innerWidth / window.innerHeight,
-    0.1,
-    1000
-);
-camera.position.z = 13;
+console.clear();
 
-const scene = new THREE.Scene();
-let bee;
-let mixer;
-const loader = new GLTFLoader();
-loader.load('/demon_bee_full_texture',
-    function (gltf) {
-        bee = gltf.scene;
-        scene.add(bee);
+// there are 3 parts to this
+//
+// Scene:           Setups and manages threejs rendering
+// loadModel:       Loads the 3d obj file
+// setupAnimation:  Creates all the GSAP 
+//                  animtions and scroll triggers 
+//
+// first we call loadModel, once complete we call
+// setupAnimation which creates a new Scene
 
-        mixer = new THREE.AnimationMixer(bee);
-        mixer.clipAction(gltf.animations[0]).play();
-        modelMove();
-    },
-    function (xhr) {},
-    function (error) {}
-);
-const renderer = new THREE.WebGLRenderer({alpha: true});
-renderer.setSize(window.innerWidth, window.innerHeight);
-document.getElementById('container3D').appendChild(renderer.domElement);
+class Scene
+{
+	constructor(model)
+	{
+		this.views = [
+			{ bottom: 0, height: 1 },
+			{ bottom: 0, height: 0 }
+		];
+		
+		this.renderer = new THREE.WebGLRenderer({
+			antialias: true,
+			alpha: true
+		});
+		
+		this.renderer.setSize(window.innerWidth, window.innerHeight);
+		this.renderer.shadowMap.enabled = true;
+		this.renderer.shadowMap.type = THREE.PCFSoftShadowMap;
+		this.renderer.setPixelRatio(window.devicePixelRatio);
 
-// light
-const ambientLight = new THREE.AmbientLight(0xffffff, 1.3);
-scene.add(ambientLight);
+		document.body.appendChild( this.renderer.domElement );
+		
+		// scene
 
-const topLight = new THREE.DirectionalLight(0xffffff, 1);
-topLight.position.set(500, 500, 500);
-scene.add(topLight);
+		this.scene = new THREE.Scene();
+		
+		for ( var ii = 0; ii < this.views.length; ++ ii ) {
 
+			var view = this.views[ ii ];
+			var camera = new THREE.PerspectiveCamera( 45, window.innerWidth / window.innerHeight, 1, 2000 );
+			camera.position.fromArray([0, 0, 180] );
+			camera.layers.disableAll();
+			camera.layers.enable( ii );
+			view.camera = camera;
+			camera.lookAt(new THREE.Vector3(0, 5, 0));
+		}
+		
+		//light
 
-const reRender3D = () => {
-    requestAnimationFrame(reRender3D);
-    renderer.render(scene, camera);
-    if(mixer) mixer.update(0.02);
-};
-reRender3D();
+		this.light = new THREE.PointLight( 0xffffff, 0.75 );
+		this.light.position.z = 150;
+		this.light.position.x = 70;
+		this.light.position.y = -20;
+		this.scene.add( this.light );
 
-let arrPositionModel = [
-    {
-        id: 'banner',
-        position: {x: 0, y: -1, z: 0},
-        rotation: {x: 0, y: 1.5, z: 0}
-    },
-    {
-        id: "intro",
-        position: { x: 1, y: -1, z: -5 },
-        rotation: { x: 0.5, y: -0.5, z: 0 },
-    },
-    {
-        id: "description",
-        position: { x: -1, y: -1, z: -5 },
-        rotation: { x: 0, y: 0.5, z: 0 },
-    },
-    {
-        id: "contact",
-        position: { x: 0.8, y: -1, z: 0 },
-        rotation: { x: 0.3, y: -0.5, z: 0 },
-    },
-];
-const modelMove = () => {
-    const sections = document.querySelectorAll('.section');
-    let currentSection;
-    sections.forEach((section) => {
-        const rect = section.getBoundingClientRect();
-        if (rect.top <= window.innerHeight / 3) {
-            currentSection = section.id;
-        }
-    });
-    let position_active = arrPositionModel.findIndex(
-        (val) => val.id == currentSection
-    );
-    if (position_active >= 0) {
-        let new_coordinates = arrPositionModel[position_active];
-        gsap.to(bee.position, {
-            x: new_coordinates.position.x,
-            y: new_coordinates.position.y,
-            z: new_coordinates.position.z,
-            duration: 3,
-            ease: "power1.out"
-        });
-        gsap.to(bee.rotation, {
-            x: new_coordinates.rotation.x,
-            y: new_coordinates.rotation.y,
-            z: new_coordinates.rotation.z,
-            duration: 3,
-            ease: "power1.out"
-        })
-    }
-}
-window.addEventListener('scroll', () => {
-    if (bee) {
-        modelMove();
-    }
-})
-window.addEventListener('resize', () => {
-    renderer.setSize(window.innerWidth, window.innerHeight);
-    camera.aspect = window.innerWidth / window.innerHeight;
-    camera.updateProjectionMatrix();
-})
+		this.softLight = new THREE.AmbientLight( 0xffffff, 1.5 );
+		this.scene.add(this.softLight)
 
+		// group
 
+		this.onResize();
+		window.addEventListener( 'resize', this.onResize, false );
+		
+		var edges = new THREE.EdgesGeometry( model.children[ 0 ].geometry );
+		let line = new THREE.LineSegments( edges );
+		line.material.depthTest = false;
+		line.material.opacity = 0.5;
+		line.material.transparent = true;
+		line.position.x = 0.5;
+		line.position.z = -1;
+		line.position.y = 0.2;	
+		
+		this.modelGroup = new THREE.Group();
+		
+		model.layers.set( 0 );
+		line.layers.set( 1 );
+			
+		this.modelGroup.add(model);
+		this.modelGroup.add(line);
+		this.scene.add(this.modelGroup);
+	}
+	
+	render = () =>
+	{
+		for ( var ii = 0; ii < this.views.length; ++ ii ) {
 
+			var view = this.views[ ii ];
+			var camera = view.camera;
 
+			var bottom = Math.floor( this.h * view.bottom );
+			var height = Math.floor( this.h * view.height );
 
+			this.renderer.setViewport( 0, 0, this.w, this.h );
+			this.renderer.setScissor( 0, bottom, this.w, height );
+			this.renderer.setScissorTest( true );
 
+			camera.aspect = this.w / this.h;
+			this.renderer.render( this.scene, camera );
+		}
+	}
+	
+	onResize = () => 
+	{
+		this.w = window.innerWidth;
+		this.h = window.innerHeight;
+		
+		for ( var ii = 0; ii < this.views.length; ++ ii ) {
+			var view = this.views[ ii ];
+			var camera = view.camera;
+			camera.aspect = this.w / this.h;
+			let camZ = (screen.width - (this.w * 1)) / 3;
+			camera.position.z = camZ < 180 ? 180 : camZ;
+			camera.updateProjectionMatrix();
+		}
 
-
-
-
-
-
-
-
-
-
-
-//step 1: get DOM
-let nextDom = document.getElementById('next');
-let prevDom = document.getElementById('prev');
-
-let carouselDom = document.querySelector('.carousel');
-let SliderDom = carouselDom.querySelector('.carousel .list');
-let thumbnailBorderDom = document.querySelector('.carousel .thumbnail');
-let thumbnailItemsDom = thumbnailBorderDom.querySelectorAll('.item');
-let timeDom = document.querySelector('.carousel .time');
-
-thumbnailBorderDom.appendChild(thumbnailItemsDom[0]);
-let timeRunning = 3000;
-let timeAutoNext = 7000;
-
-nextDom.onclick = function(){
-    showSlider('next');    
+		this.renderer.setSize( this.w, this.h );		
+		this.render();
+	}
 }
 
-prevDom.onclick = function(){
-    showSlider('prev');    
-}
-let runTimeOut;
-let runNextAuto = setTimeout(() => {
-    next.click();
-}, timeAutoNext)
-function showSlider(type){
-    let  SliderItemsDom = SliderDom.querySelectorAll('.carousel .list .item');
-    let thumbnailItemsDom = document.querySelectorAll('.carousel .thumbnail .item');
-    
-    if(type === 'next'){
-        SliderDom.appendChild(SliderItemsDom[0]);
-        thumbnailBorderDom.appendChild(thumbnailItemsDom[0]);
-        carouselDom.classList.add('next');
-    }else{
-        SliderDom.prepend(SliderItemsDom[SliderItemsDom.length - 1]);
-        thumbnailBorderDom.prepend(thumbnailItemsDom[thumbnailItemsDom.length - 1]);
-        carouselDom.classList.add('prev');
-    }
-    clearTimeout(runTimeOut);
-    runTimeOut = setTimeout(() => {
-        carouselDom.classList.remove('next');
-        carouselDom.classList.remove('prev');
-    }, timeRunning);
+function loadModel() 
+{
+	gsap.registerPlugin(ScrollTrigger);
+	gsap.registerPlugin(DrawSVGPlugin);
+	gsap.set('#line-length', {drawSVG: 0})
+	gsap.set('#line-wingspan', {drawSVG: 0})
+	gsap.set('#circle-phalange', {drawSVG: 0})
+	
+	var object;
 
-    clearTimeout(runNextAuto);
-    runNextAuto = setTimeout(() => {
-        next.click();
-    }, timeAutoNext)
+	function onModelLoaded() {
+		object.traverse( function ( child ) {
+			let mat = new THREE.MeshPhongMaterial( { color: 0x171511, specular: 0xD0CBC7, shininess: 5, flatShading: true } );
+			child.material = mat;
+		});
+
+		setupAnimation(object);
+	}
+
+	var manager = new THREE.LoadingManager( onModelLoaded );
+	manager.onProgress = ( item, loaded, total ) => console.log( item, loaded, total );
+
+	var loader = new THREE.OBJLoader( manager );
+	loader.load( 'https://assets.codepen.io/557388/1405+Plane_1.obj', function ( obj ) { object = obj; });
 }
+
+function setupAnimation(model)
+{
+	let scene = new Scene(model);
+	let plane = scene.modelGroup;
+	
+	gsap.fromTo('canvas',{x: "50%", autoAlpha: 0},  {duration: 1, x: "0%", autoAlpha: 1});
+	gsap.to('.loading', {autoAlpha: 0})
+	gsap.to('.scroll-cta', {opacity: 1})
+	gsap.set('svg', {autoAlpha: 1})
+	
+	let tau = Math.PI * 2;
+
+	gsap.set(plane.rotation, {y: tau * -.25});
+	gsap.set(plane.position, {x: 80, y: -32, z: -60});
+	
+	scene.render();
+	
+	var sectionDuration = 1;
+	gsap.fromTo(scene.views[1], 
+		{ 	height: 1, bottom: 0 }, 
+		{
+			height: 0, bottom: 1,
+			ease: 'none',
+			scrollTrigger: {
+			  trigger: ".blueprint",
+			  scrub: true,
+			  start: "bottom bottom",
+			  end: "bottom top"
+			}
+		})
+	
+	gsap.fromTo(scene.views[1], 
+		{ 	height: 0, bottom: 0 }, 
+		{
+			height: 1, bottom: 0,
+			ease: 'none',
+			scrollTrigger: {
+			  trigger: ".blueprint",
+			  scrub: true,
+			  start: "top bottom",
+			  end: "top top"
+			}
+		})
+	
+	gsap.to('.ground', {
+		y: "30%",
+		scrollTrigger: {
+		  trigger: ".ground-container",
+		  scrub: true,
+		  start: "top bottom",
+		  end: "bottom top"
+		}
+	})
+	
+	gsap.from('.clouds', {
+		y: "25%",
+		scrollTrigger: {
+		  trigger: ".ground-container",
+		  scrub: true,
+		  start: "top bottom",
+		  end: "bottom top"
+		}
+	})
+	
+	gsap.to('#line-length', {
+		drawSVG: 100,
+		scrollTrigger: {
+		  trigger: ".length",
+		  scrub: true,
+		  start: "top bottom",
+		  end: "top top"
+		}
+	})
+	
+	gsap.to('#line-wingspan', {
+		drawSVG: 100,
+		scrollTrigger: {
+		  trigger: ".wingspan",
+		  scrub: true,
+		  start: "top 25%",
+		  end: "bottom 50%"
+		}
+	})	
+	
+	gsap.to('#circle-phalange', {
+		drawSVG: 100,
+		scrollTrigger: {
+		  trigger: ".phalange",
+		  scrub: true,
+		  start: "top 50%",
+		  end: "bottom 100%"
+		}
+	})
+	
+	gsap.to('#line-length', {
+		opacity: 0,
+		drawSVG: 0,
+		scrollTrigger: {
+		  trigger: ".length",
+		  scrub: true,
+		  start: "top top",
+		  end: "bottom top"
+		}
+	})
+	
+	gsap.to('#line-wingspan', {
+		opacity: 0,
+		drawSVG: 0,
+		scrollTrigger: {
+		  trigger: ".wingspan",
+		  scrub: true,
+		  start: "top top",
+		  end: "bottom top"
+		}
+	})	
+	
+	gsap.to('#circle-phalange', {
+		opacity: 0,
+		drawSVG: 0,
+		scrollTrigger: {
+		  trigger: ".phalange",
+		  scrub: true,
+		  start: "top top",
+		  end: "bottom top"
+		}
+	})
+	
+	let tl = new gsap.timeline(
+	{
+		onUpdate: scene.render,
+		scrollTrigger: {
+		  trigger: ".content",
+		  scrub: true,
+		  start: "top top",
+		  end: "bottom bottom"
+		},
+		defaults: {duration: sectionDuration, ease: 'power2.inOut'}
+	});
+	
+	let delay = 0;
+	
+	tl.to('.scroll-cta', {duration: 0.25, opacity: 0}, delay)
+	tl.to(plane.position, {x: -10, ease: 'power1.in'}, delay)
+	
+	delay += sectionDuration;
+	
+	tl.to(plane.rotation, {x: tau * .25, y: 0, z: -tau * 0.05, ease: 'power1.inOut'}, delay)
+	tl.to(plane.position, {x: -40, y: 0, z: -60, ease: 'power1.inOut'}, delay)
+	
+	delay += sectionDuration;
+	
+	tl.to(plane.rotation, {x: tau * .25, y: 0,  z: tau * 0.05, ease: 'power3.inOut'}, delay)
+	tl.to(plane.position, {x: 40, y: 0, z: -60, ease: 'power2.inOut'}, delay)
+	
+	delay += sectionDuration;
+	
+	tl.to(plane.rotation, {x: tau * .2, y: 0, z: -tau * 0.1, ease: 'power3.inOut'}, delay)
+	tl.to(plane.position, {x: -40, y: 0, z: -30, ease: 'power2.inOut'}, delay)
+	
+	delay += sectionDuration;
+	
+	tl.to(plane.rotation, { x: 0, z: 0, y: tau * .25}, delay)
+	tl.to(plane.position, { x: 0, y: -10, z: 50}, delay)
+	
+	delay += sectionDuration;
+	delay += sectionDuration;
+	
+	tl.to(plane.rotation, {x: tau * 0.25, y: tau *.5, z: 0, ease:'power4.inOut'}, delay)
+	tl.to(plane.position, {z: 30, ease:'power4.inOut'}, delay)
+	
+	delay += sectionDuration;
+	
+	tl.to(plane.rotation, {x: tau * 0.25, y: tau *.5, z: 0, ease:'power4.inOut'}, delay)
+	tl.to(plane.position, {z: 60, x: 30, ease:'power4.inOut'}, delay)
+	
+	delay += sectionDuration;
+	
+	tl.to(plane.rotation, {x: tau * 0.35, y: tau *.75, z: tau * 0.6, ease:'power4.inOut'}, delay)
+	tl.to(plane.position, {z: 100, x: 20, y: 0, ease:'power4.inOut'}, delay)
+	
+	delay += sectionDuration;
+	
+	tl.to(plane.rotation, {x: tau * 0.15, y: tau *.85, z: -tau * 0, ease: 'power1.in'}, delay)
+	tl.to(plane.position, {z: -150, x: 0, y: 0, ease: 'power1.inOut'}, delay)
+	
+	delay += sectionDuration;
+	
+	tl.to(plane.rotation, {duration: sectionDuration, x: -tau * 0.05, y: tau, z: -tau * 0.1, ease: 'none'}, delay)
+	tl.to(plane.position, {duration: sectionDuration, x: 0, y: 30, z: 320, ease: 'power1.in'}, delay)
+	
+	tl.to(scene.light.position, {duration: sectionDuration, x: 0, y: 0, z: 0}, delay)
+}
+
+loadModel();
